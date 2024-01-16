693e92d91b08d286e8f0aed6a7129dea73db1fc0
Arthur <127118625+IORU4H3@users.noreply.github.com>
2024-01-16 11:50:23 +0100

    Add files via upload

--- unitcuisineihm.pas
+++ unitcuisineihm.pas
@@ -1,33 +1,41 @@
 unit UnitCuisineIHM;
 
 {$mode objfpc}{$H+}
+{$codepage utf8}
 
 interface
 uses
   UnitDeplacement;
 
-function EcranCuisine(): TLieu;
-procedure AfficherRecette(numRecette: integer; position: integer);
+function EcranCuisine(): TLieu; // écran général de la cuisine
+procedure AfficherRecette(numRecette: integer; position: integer); // on affiche le nom et les ingredients des recettes
+Procedure PageRecettes(num: integer); // correpond à une page de recette (3 recettes par pages)
 
 
 implementation
-uses GestionEcran, UnitIHM;
+uses UnitIHM, SysUtils, UnitRecette;
 
 
 procedure AfficherRecette(numRecette: integer; position: integer);
 var
   i : integer;
 begin
-  if (numRecette <= GetNombreRecettes()) and (numRecette>0) then
+  if (numRecette <= GetNombreRecettes()) and (numRecette>0) then // si la recette existe
   begin
-    EcrireDescription(' '+IntToStr(position)+GetNomRectte());
+
+    // on affiche la position et le nom de la recette
+    EcrireDescription(' '+IntToStr(position)+'- '+GetNomRecette(numRecette));
+
+    for i := 1 to 3 do // au max 3 ingredients
+        if  GetNomIngredient(numRecette, i) <> '' then // s'il y a des ingredients, on les affiches
+            EcrireDescription('   '+GetNomIngredient(numRecette, i));
   end;
 end;
 
-function EcranCuisine(): TLieu;
+function EcranCuisine(): TLieu; // écran principal
 begin
   PageRecettes(1);
-  EcranCuisine := MAISON;
+  EcranCuisine := MAISON; // la cuisine est dans la maison
 end;
 
 Procedure PageRecettes(num: integer);
@@ -36,18 +44,22 @@ var
   i: integer;
 begin
   choix := -1;
-  while (choix <> 0) then
+  while (choix <> 0) do // temps qu'il y a des recettes à afficher
   begin
+
     BaseIHM('Lieu : La cuisine');
     AffichageMeteo();
 
     EcrireDescriptionAlert('Liste des recettes');
 
 
-    for i := 1 to 4 do
+    for i := 1 to 4 do  // on affiche les recettes
     begin
       EcrireDescription('');
-      AfficherRecette(num+i-1,i);
+      if num+i = GetNombreRecettes+2  then   // si on a plus de recettes, on arrête
+         choix := 0
+      else
+          AfficherRecette(num+i-1,i);
     end;
 
     EcrireMenu(' 2- page suivante');
@@ -58,11 +70,11 @@ begin
 
     SaisirChoix(choix);
 
+    // les pages suivantes / precedantes.
     case choix of
-    1: PageRecette(num-4);
-    2: PageRecette(num+4);
+      1: PageRecettes(num-4);
+      2: PageRecettes(num+4);
     end;
-
 
   end;
 
